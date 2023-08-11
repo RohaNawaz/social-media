d67ee6b03c9fcfd385a0ca704d2e353796179da2
RohaNawaz <101508514+RohaNawaz@users.noreply.github.com>
2023-08-11 17:57:31 +0500

    sidebar & postcard

---
+++ components/Avatar.js
@@ -0,0 +1,7 @@
+export default function Avatar() {
+    return (
+        <div className="w-20 rounded-full overflow-hidden">
+            <img src="https://media.istockphoto.com/id/1399395748/photo/cheerful-business-woman-with-glasses-posing-with-her-hands-under-her-face-showing-her-smile.webp?b=1&s=170667a&w=0&k=20&c=HdGlQ9BzmRtM015ck_F3PCIlB-_7id0Lp4h2rKLIj-0="></img>
+        </div>
+    )
+}
