c6bbd3fab8c19819c03dcf8737c46fbd5810101c
RohaNawaz <101508514+RohaNawaz@users.noreply.github.com>
2023-08-12 15:38:05 +0500

    single post

--- app/page.js
+++ app/page.js
@@ -1,17 +1,19 @@
 import Card from '@/components/Card'
 import NavigationCard from '@/components/NavigationCard'
 import PostFormCard from '@/components/PostFormCard'
+import Avatar from '@/components/Avatar'
+import PostCard from '@/components/PostCard'
 
 export default function Home() {
   return (
     <div className="flex mt-4 max-w-4xl mx-auto gap-6">
-      <div className="w-1/3">
+      <div className="w-3/12">
       <NavigationCard />
       </div>
 
-      <div className="grow">
+      <div className="9/12">
         <PostFormCard />
-        <Card>first post here</Card>
+        <PostCard />
       </div>
     </div>
   )
