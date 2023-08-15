a44ac58657e85f7741b8b2428f190a4fcd462c80
RohaNawaz <101508514+RohaNawaz@users.noreply.github.com>
2023-08-15 23:37:57 +0500

    login half

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,15 @@
 import NavigationCard from "./NavigationCard"
 
-export default function Layout({children}) {
+export default function Layout({children, hideNavigation}) {
     return (
         <div className="flex mt-4 max-w-4xl mx-auto gap-6">
-      <div className="w-3/12">
-      <NavigationCard />
-      </div>
+          {!hideNavigation && (
+            <div className="w-3/12">
+            <NavigationCard />
+            </div>
+          )}
 
-      <div className="9/12">
+      <div className={hideNavigation ? 'w-full' : "9/12"}>
         {children}
       </div>
     </div>
