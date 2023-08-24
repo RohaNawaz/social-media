369edfcb61169f99621838542d5d6df6b49fd454
RohaNawaz <101508514+RohaNawaz@users.noreply.github.com>
2023-08-24 19:15:39 +0500

    mini change

--- app/Profile/page.js
+++ app/Profile/page.js
@@ -16,6 +16,8 @@ export default function ProfilePage() {
   const router = useRouter();
   const userId = router.query.id;
   const [profile, setProfile] = useState(null);
+  const [editMode, setEditMode] = useState(null);
+  console.log(profile);
   const supabase = useSupabaseClient();
   const session = useSession();
 
@@ -62,12 +64,58 @@ export default function ProfilePage() {
                 )}
               </div>
 
-              <div className="p-4 pb-0">
-                <div className="ml-40">
-                  <h1 className="text-3xl font-bold">
-                   {profile?.name}
-                  </h1>
-                  <div>{profile?.places}</div>
+              <div className="p-4 pb-0 pt-0 md:pt-4">
+                <div className="ml-24 md:ml-40 flex justify-between">
+                  <div>
+                    {editMode && (
+                    <div>
+                      <input type="text"
+                             className="border py-2 px-3 rounded-md"
+                             placeholder={'Your name'}
+                             onChange={ev => setName(ev.target.value)}
+                             value={name}/>
+                    </div>
+                  )}
+                   {!editMode && (
+                    <h1 className="text-3xl font-bold">
+                      {profile?.name}
+                    </h1>
+                  )}
+                    {!editMode && (
+                    <div className="text-gray-500 leading-4">
+                      {profile?.place || 'Internet'}
+                    </div>
+                  )}
+                  {editMode && (
+                    <div>
+                      <input type="text"
+                             className="border py-2 px-3 rounded-md mt-1"
+                             placeholder={'Your location'}
+                             onChange={ev => setPlace(ev.target.value)}
+                             value={place}/>
+                    </div>
+                  )}
+                  </div>
+                  {isMyUser && !editMode && (
+                    <div>
+                      <button onClick={() => {
+                          setEditMode(true);
+                          setName(profile.name);
+                          setPlace(profile.place);
+                        }} 
+                        className="inline-flex mx-1 gap-1 bg-white rounded-md shadow-sm shadow-gray-500 py-1 px-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                        </svg>
+                        Edit Profile
+                      </button>
+                  </div>
+                  )}
+                  {isMyUser && editMode && (
+                      <button onClick={saveProfile} className="inline-flex mx-1 gap-1 bg-white rounded-md shadow-sm shadow-gray-500 py-1 px-2">
+                        Save profile
+                      </button>
+                    )}
                 </div>
                 <div className="mt-10 flex gap-0">
                   <Link href={'/posts'} className={isPosts ? activeTabClasses : tabClasses}>
