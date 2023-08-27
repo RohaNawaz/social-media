a89b59a780cf5a6f8e0a2ecb11a420f8b5013e88
RohaNawaz <101508514+RohaNawaz@users.noreply.github.com>
2023-08-27 15:57:45 +0500

    cover

--- app/profile1/page.js
+++ app/profile1/page.js
@@ -1,161 +1,3 @@
-// 'use client'
-
-// export const dynamic = 'force-dynamic';
-// import Avatar from "@/components/Avatar";
-// import Card from "@/components/Card";
-// import Cover from "@/components/Cover";
-// import Layout from "@/components/Layout";
-// import PostCard from "@/components/PostCard";
-// import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
-// import { useRouter } from "next/navigation";
-// import { useEffect, useState } from "react";
-
-// export default function ProfilePage() {
-//   const router = useRouter();
-//   console.log(router);
-//   const userId = router?.query?.id;
-//   const [profile, setProfile] = useState(null);
-//   const [editMode, setEditMode] = useState(null);
-//   const [name, setName] = useState('');
-//   const [place, setPlace] = useState('');
-//   console.log(profile);
-//   const supabase = useSupabaseClient();
-//   const session = useSession();
-  
-
-//   useEffect(() => {
-//       if(!userId) {
-//         return;
-//       }
-//       fetchUser(); 
-//   }, [userId]);
-
-//   function fetchUser() {
-//     supabase.from('profiles')
-//      .select()
-//      .eq('id', userId)
-//      .then(result => {
-//        if(result.error) {
-//         throw result.error;
-//        }
-//        if(result.data) {
-//         setProfile(result.data[0]);
-//        }
-//       });
-//   }
-
-//   function saveProfile() {
-//     supabase.from('profiles')
-//       .update({
-//         name,
-//         place,
-//       })
-//       .eq('id', session?.user?.id)
-//       .then(result => {
-//         if (!result.error) {
-//           setProfile(prev => ({...prev,name,place}));
-//         }
-//         setEditMode(false);
-//       });
-//   }
-
-//   const isMyUser = userId === session?.user?.id;
-
-//     return (
-//         <Layout>
-//             <Card noPadding={true}> 
-//             <div className="relative overflow-hidden rounded-md ">
-//               <Cover url={profile?.cover} 
-//               editable={isMyUser} 
-//               onChange={fetchUser} 
-//               />
-
-//               <div className="absolute top-14 left-4 z-20">
-//                 {profile && (
-//                   <Avatar url={profile?.avatar} size={'lg'} 
-//                   editable={isMyUser} 
-//                   onChange={fetchUser}
-//                   />
-//                 )}
-//               </div>
-
-//               <div className="p-4 pb-0 pt-0 md:pt-4">
-//                 <div className="ml-24 md:ml-40 flex justify-between">
-//                   <div>
-//                     {editMode && (
-//                     <div>
-//                       <input type="text"
-//                              className="border py-2 px-3 rounded-md"
-//                              placeholder={'Your name'}
-//                              onChange={ev => setName(ev.target.value)}
-//                              value={name}/>
-//                     </div>
-//                   )}
-//                    {!editMode && (
-//                     <h1 className="text-3xl font-bold">
-//                       {profile?.name}
-//                     </h1>
-//                   )}
-//                     {!editMode && (
-//                     <div className="text-gray-500 leading-4">
-//                       {profile?.place || 'Internet'}
-//                     </div>
-//                   )}
-//                   {editMode && (
-//                     <div>
-//                       <input type="text"
-//                              className="border py-2 px-3 rounded-md mt-1"
-//                              placeholder={'Your location'}
-//                              onChange={ev => setPlace(ev.target.value)}
-//                              value={place}/>
-//                     </div>
-//                   )}
-//                   </div>
-//                   <div className="grow">
-//                     <div className="text-right">
-//                     {isMyUser && !editMode && (
-//                     <div>
-//                       <button onClick={() => {
-//                           setEditMode(true);
-//                           setName(profile?.name);
-//                           setPlace(profile?.place);
-//                         }} 
-//                         className="inline-flex mx-1 gap-1 bg-white rounded-md shadow-sm shadow-gray-500 py-1 px-2">
-//                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-//                           <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-//                         </svg>
-//                         Edit Profile
-//                       </button>
-//                   </div>
-//                   )}
-//                   {isMyUser && editMode && (
-//                       <button 
-//                       onClick={saveProfile} 
-//                       className="inline-flex mx-1 gap-1 bg-white rounded-md shadow-sm shadow-gray-500 py-1 px-2">
-//                         Save profile
-//                       </button>
-//                     )}
-//                     {isMyUser && editMode && (
-//                       <button onClick={() => setEditMode(false)} className="inline-flex mx-1 gap-1 bg-white rounded-md shadow-sm shadow-gray-500 py-1 px-2">
-//                         Cancel
-//                       </button>
-//                     )}
-//                     </div> 
-//                    </div>
-//                 </div>
-
-
-                
-
-//             </div>
-//             </div>
-//             </Card>
-
-//             <PostCard />
-//         </Layout>
-//     )
-// }
-
 'use client';
 
 import Layout from "@/components/Layout";
@@ -167,7 +9,7 @@ import {useRouter} from "next/navigation";
 import {useEffect, useState} from "react";
 import {useSession, useSupabaseClient} from "@supabase/auth-helpers-react";
 import Cover from "@/components/Cover";
-// import {UserContextProvider} from "@/contexts/UserContext";
+// import {UserContext} from "@/contexts/UserContext";
 
 export default function ProfilePage() {
   const [profile,setProfile] = useState(null);
@@ -207,7 +49,7 @@ export default function ProfilePage() {
         name,
         place,
       })
-      .eq('id', session.user.id)
+      .eq('id', session?.user?.id)
       .then(result => {
         if (!result.error) {
           setProfile(prev => ({...prev,name,place}));
@@ -221,13 +63,13 @@ export default function ProfilePage() {
 
   return (
     <Layout>
-      {/* <UserContextProvider> */}
+      {/* <UserContext> */}
         <Card noPadding={true}>
           <div className="relative overflow-hidden rounded-md">
             <Cover url={profile?.cover} editable={isMyUser} onChange={fetchUser} />
             <div className="absolute top-24 left-4 z-20">
               {profile && (
-                <Avatar url={profile.avatar} size={'lg'} editable={isMyUser} onChange={fetchUser} />
+                <Avatar url={profile?.avatar} size={'lg'} editable={isMyUser} onChange={fetchUser} />
               )}
             </div>
             <div className="p-4 pt-0 md:pt-4 pb-0">
@@ -294,7 +136,7 @@ export default function ProfilePage() {
             </div>
           </div>
         </Card>
-      {/* </UserContextProvider> */}
+      {/* </UserContext> */}
       <PostCard />
     </Layout>
   );
